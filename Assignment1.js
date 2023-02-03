ae4bc88153f88793868dc8204b38b940ef425cfd
TejasKumar0215 <124339629+TejasKumar0215@users.noreply.github.com>
2023-02-03 16:52:39 +0530

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,58 @@
+/*1.Declare a variable named challenge and assign it to an initial value 'Javascript with Bharath'.*/
+
+var name='Javascript with Bharath'
+
+
+/*2.Print the string on the browser console using console.log()*/
+
+let first_name = 'Tejas Kumar'
+let last_name = 'Nagaraj'
+let place = 'Kanakapura'
+let full_name= first_name + ' ' + last_name
+
+let bio='Hey I am '+ full_name + ',' + 'I am from '+ place 
+
+console.log(bio)
+
+/*3.Print the length of the string on the browser console using console.log()*/
+
+let father_name='Nagaraju  '
+
+console.log(father_name.lenght)
+
+
+
+
+/*4.Change all the string characters to capital letters using toUpperCase() method*/
+
+let country = "India"
+
+country.toUpperCase()
+
+/*5.Change all the string characters to lowercase letters using toLowerCase() method*/
+country.toLowerCase()
+
+
+
+/*6.Cut (slice) out the first word of the string using substr() or substring() method*/
+
+output = country.substr(1,4)
+
+
+
+/*7.Slice out the phrase JavaScript With from Javascript with Bharath.*/
+
+//output = name.substr(10,21)
+
+/*8.Check if the string contains a word Script using includes() method*/
+
+
+
+
+
+
+
+
+
+
+
---
+++ T1.js
@@ -0,0 +1,11 @@
+let str1='Rs20000'
+let str2='20'
+let str3='India'
+let str4='Karnataka'
+let str5='kannada'
+let str6=`Amid the ongoing Adani Enterprises rout, Opposition Congress on 2 February announced it will hold nationwide protests in front of Life Insurance Corporation Offices and State Bank of India branches in all districts on February 6.\
+"Government at the Centre is using common people's money to support their closest friends. Congress party has decided to have nationwide agitation across districts of the country in front of LIC and SBI offices on Monday," 
+ ANI quoted Congress general secretary KC Venugopal as saying.
+According to Congress, the exposure of LIC and public sector banks like the SBI to the Adani Group has significant implications for middle-class savings.
+\
+Meanwhile, the opposition leaders are expecting the government to brazen it out and not concede their demand. So they will try to use the Parliament stage to corner Modi.`
