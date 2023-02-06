835e309256aadd23068680c9c8adc8ab87e5de86
TejasKumar0215 <124339629+TejasKumar0215@users.noreply.github.com>
2023-02-06 17:23:25 +0530

    Add files via upload

--- Assignment1.js
+++ Assignment1.js
@@ -36,7 +36,9 @@ country.toLowerCase()
 
 /*6.Cut (slice) out the first word of the string using substr() or substring() method*/
 
-output = country.substr(1,4)
+//output = country.substr(1,4)
+
+//output = country.Substring(1,2)
 
 
 
@@ -44,15 +46,116 @@ output = country.substr(1,4)
 
 //output = name.substr(10,21)
 
+
 /*8.Check if the string contains a word Script using includes() method*/
+	
+string = 'Taking turns answering questions, Mueller addresses the correct use of the lastmod tag, while Illyes discusses the benefits of separate sitemaps'
+
+//output = string.includes('correct')
+
+
+/*9.Split the string into an array using split() method*/
+
+/*str8 = 'Acceleration'*/
+/*output = str8.split('')*/
+/*console.log(output)/*
+
+
+/*10.Split the string Javascript with Bharath at the space using split() method*/
+
+//output = name.split(' ')
+
+
+/*'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.*/
+
+str9 = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+//output= str9.split(',')
+
+/*11.Change Javascript with Bharath to Mastering Python with Bharath using replace() method.*/
+
+//output = name.replace('Javascript with Bharath','Mastering Python with Bharath')
+
+/*12.What is character at index 15 in 'Javascript with Bharath' string? Use charAt() method.*/
+
+//output = name.charAt((16))
 
 
 
 
 
+/*13.What is the character code of J in 'Javascript with Bharath' string using charCodeAt()*/
+
+//output = name.charCodeAt(('v'))
+
+
+/*14.Use indexOf to determine the position of the first occurrence of a in Javascript with Bharath*/
+
+//output = name.indexOf('a')
+
+/*15.Use lastIndexOf to determine the position of the last occurrence of a in Javascript with Bharath.*/
+
+//output = name.lastIndexOf('a')
+
+/*16.Use indexOf to find the position of the first occurrence of the word because in the following sentence:*/
+
+str10 = 'You cannot end a sentence with because because because is a conjunction'
+
+//output = str10.indexOf('because')
+
+/* 17.Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:*/
+
+output = str10.lastIndexOf('because')
+
+/*18.Use search to find the position of the first occurrence of the word because in the following sentence:*/
+
+output = str10.search('because')
+
+/*19.Use trim() to remove any trailing whitespace at the beginning and the end of a string.*/
+
+str11 = ' Javascript with Bharath '
+output = str11.trim()
+
+/*20.Use startsWith() method with the string Javascript with Bharath and make the result true*/
+
+output = name.startsWith('Java')
+
+
+/*21.Use endsWith() method with the string Javascript with Bharath and make the result true*/
+
+output = name.endsWith('ath')
+
+
+/*22.Use match() method to find all the a’s in Javascript with Bharath*/
+
+output = name.match('a')
+
+/*23.Use concat() and merge 'JavaScript' and 'with Bharath' to a single string, 'Javascript with Bharath'*/
 
 
 
 
+/*24.Use repeat() method to print Javascript with Bharath 2 times*/
+
+output = name.repeat()
 
 
+/*25.Using console.log() print out the following statement:*/
+
+str12 = "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+
+
+
+
+Using console.log() print out the following quote by Mother Teresa:
+Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+Use console.log() and escape characters to print the following pattern.
+
+1 1     1 1 1
+2   1 2 4     8
+3 1     3   9 27
+
+
+
+
+Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+   
